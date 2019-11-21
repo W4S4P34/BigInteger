a6aa58a914f064693f56752619bb90ad77d1be11
Thành Thái La <18127207@student.hcmus.edu.vn>
2019-11-22 00:56:05 +0700

    Update operators prototype

--- BigInteger/18127087_18127207/QInt.h
+++ BigInteger/18127087_18127207/QInt.h
@@ -8,8 +8,39 @@ using namespace std;
 class QInt
 {
 private:
-	bitset<128> arrayBits; 
+	bitset<128> arrayBits;
 
 public:
+	// Constructors
+	QInt();
+	QInt(const QInt&);
 
+	// Base Conversion Methods
+	void toBase2(string base);
+	string toBase10();
+	string toBase16();
+
+	// Unary Operators
+	// Support Operators
+	QInt& operator=(const QInt&);
+
+	// Bitwise
+	QInt operator~();
+
+	// Binary Operators
+	// Arithmetic
+	QInt operator+(const QInt&);
+	QInt operator-(const QInt&);
+	QInt operator*(const QInt&);
+	QInt operator/(const QInt&);
+
+	// Bitwise
+	QInt operator<<(const int8_t&);
+	QInt operator>>(const int8_t&);
+	QInt rol();
+	QInt ror();
+
+	QInt operator& (const QInt&);
+	QInt operator| (const QInt&);
+	QInt operator^ (const QInt&);
 };
