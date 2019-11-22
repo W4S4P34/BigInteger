a4ac00a70addff2213889b16203b721159659684
DuyDuong0602 <47823513+DuyDuong0602@users.noreply.github.com>
2019-11-22 20:57:18 +0700

    Add operator =
    A

--- BigInteger/18127087_18127207/QInt.cpp
+++ BigInteger/18127087_18127207/QInt.cpp
@@ -1,4 +1,22 @@
 #include "QInt.h"
+//Support Operators
+QInt& QInt::operator=(const QInt& a) {
+	QInt b;
+	for (int i = 0; i < a.arrayBits.size(); i++) {
+		b.arrayBits[i] = a.arrayBits[i];
+	}
+	return b;
+}
+
+//Bitwise
+QInt QInt::operator~() {
+	QInt b;
+	for (int i = 0; i < b.arrayBits.size() - 1; i++) {
+		~b.arrayBits[i];
+	}
+	return b;
+}
+
 QInt QInt::operator&(const QInt& a) {
 	QInt b;
 	QInt c;
@@ -7,6 +25,7 @@ QInt QInt::operator&(const QInt& a) {
 	}
 	return c;
 }
+
 QInt QInt::operator|(const QInt& a) { 
 	QInt b;
 	QInt c;
@@ -15,6 +34,7 @@ QInt QInt::operator|(const QInt& a) {
 	}
 	return c;
 }
+
 QInt QInt::operator^(const QInt& a) {
 	QInt b;
 	QInt c;
@@ -22,11 +42,4 @@ QInt QInt::operator^(const QInt& a) {
 		c.arrayBits[i] = b.arrayBits[i] ^ a.arrayBits[i];
 	}
 	return c;
-}
-QInt QInt::operator~() {
-	QInt b;
-	for (int i = 0; i < b.arrayBits.size() - 1; i++) {
-		~b.arrayBits[i];
-	}
-	return b;
 }
