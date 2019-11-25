9ab27f40e88ffd3b821703bd146acc52546a0831
Thành Thái La <18127207@student.hcmus.edu.vn>
2019-11-25 11:57:43 +0700

    Update operator +

--- BigInteger/18127087_18127207/QInt.cpp
+++ BigInteger/18127087_18127207/QInt.cpp
@@ -363,3 +363,38 @@ string QInt::convertBinChunkToHex(string chunk)
 }
 
 /*************************************************************************/
+
+// Binary Operators
+// Arithmetic
+QInt QInt::operator+(const QInt& Qi) {
+	bitset<1> temp = { 0 };
+	QInt tempQi;
+	for (int i = 0; i < (int)Qi.arrayBits.size(); i++) {
+		tempQi.arrayBits[i] = (this->arrayBits[i] ^ Qi.arrayBits[i]);
+		if (this->arrayBits[i] == Qi.arrayBits[i]) {
+			if (Qi.arrayBits[i] == 0) {
+				if (temp[0] == 1) {
+					tempQi.arrayBits[i] = tempQi.arrayBits[i] + temp[0];
+					temp.flip();
+				}
+				else
+					tempQi.arrayBits[i] = tempQi.arrayBits[i] + temp[0];
+			}
+			else {
+				if (temp[0] == 1)
+					tempQi.arrayBits[i] = tempQi.arrayBits[i] + temp[0];
+				else {
+					tempQi.arrayBits[i] = tempQi.arrayBits[i] + temp[0];
+					temp.flip();
+				}
+			}
+		}
+		else {
+			if (this->arrayBits[i] == 1)
+				tempQi.arrayBits[i] = this->arrayBits[i] ^ temp[0];
+			else
+				tempQi.arrayBits[i] = Qi.arrayBits[i] ^ temp[0];
+		}
+	}
+	return tempQi;
+}
--- BigInteger/18127087_18127207/QInt.h
+++ BigInteger/18127087_18127207/QInt.h
@@ -43,7 +43,7 @@ public:
 	QInt& operator=(const QInt&);
 
 	// Bitwise
-	QInt operator~();
+	//QInt operator~();
 
 	// Binary Operators
 	// Arithmetic
