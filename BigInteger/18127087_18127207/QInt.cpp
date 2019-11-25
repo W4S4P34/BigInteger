784b7c2edbe66c6f35ddfcdc29e19e6d2f5e6f6e
DuyDuong0602 <47823513+DuyDuong0602@users.noreply.github.com>
2019-11-25 11:36:22 +0700

    Chắc
    đúng

--- BigInteger/18127087_18127207/QInt.cpp
+++ BigInteger/18127087_18127207/QInt.cpp
@@ -18,11 +18,31 @@ QInt QInt::operator~() {
 QInt QInt::operator+(const QInt& Qi) {
 	bitset<1> temp = { 0 };
 	QInt tempQi;
-	for (int i = Qi.arrayBits.size() - 1; i >= 0; i--) {
-		tempQi.arrayBits[i] = (this->arrayBits[i] ^ Qi.arrayBits[i]) + temp[0];
+	for (int i = 0; i < Qi.arrayBits.size(); i++) {
+		tempQi.arrayBits[i] = (this->arrayBits[i] ^ Qi.arrayBits[i]);
 		if (this->arrayBits[i] == Qi.arrayBits[i]) {
-			if ((temp[0] == 1 && Qi.arrayBits[i] == 0) || (temp[0] == 0 && Qi.arrayBits[i] == 1)) 
-				temp.flip();
+			if (Qi.arrayBits[i] == 0) {
+				if (temp[0] == 1) {
+					tempQi.arrayBits[i] = tempQi.arrayBits[i] + temp[0];
+					temp.flip();
+				}
+				else
+					tempQi.arrayBits[i] = tempQi.arrayBits[i] + temp[0];
+			}
+			else {
+				if (temp[0] == 1)
+					tempQi.arrayBits[i] = tempQi.arrayBits[i] + temp[0];
+				else {
+					tempQi.arrayBits[i] = tempQi.arrayBits[i] + temp[0];
+					temp.flip();
+				}
+			}
+		}
+		else {
+			if (this->arrayBits[i] == 1)
+				tempQi.arrayBits[i] = this->arrayBits[i] ^ temp[0];
+			else
+				tempQi.arrayBits[i] = Qi.arrayBits[i] ^ temp[0];
 		}
 	}
 	return tempQi;
